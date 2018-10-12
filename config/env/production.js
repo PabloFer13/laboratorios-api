5c8b11870f10c49de6db2ec987ae912e5b4222df
Pablo Melendez <pablofer.melendez@gmail.com>
2018-10-12 13:34:51 -0500

    fix again

--- config/env/production.js
+++ config/env/production.js
@@ -251,7 +251,7 @@ module.exports = {
     *                                                                          *
     ***************************************************************************/
     onlyAllowOrigins: [
-      'https://laboratorios-api.herokuapp.com/'
+      'https://laboratorios-api.herokuapp.com'
     ],
 
 
